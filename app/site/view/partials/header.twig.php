85466c0465b1b8efff3d4eadef5a6d620884295c
skyd3ath <skyd3ath@users.noreply.github.com>
2020-07-16 21:59:18 -0300

    alterando o input de pesquisa no header

--- app/site/view/partials/header.twig.php
+++ app/site/view/partials/header.twig.php
@@ -2,7 +2,7 @@
             <div class="max-width">
               <nav class="navbar navbar-expand-lg navbar-dark bg-info">
                 <a class="navbar-brand" href="{{BASE}}"><img src="{{BASE}}assets/img/logo.png" class="logo"></a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navmenu">
+                <button class="navbar-toggler" data-toggle="collapse" data-target="#navmenu">
                   <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -11,9 +11,14 @@
                     <li class="nav-item active">
                       <a class="nav-link novareceita" href="{{BASE}}?url=nova">Cadastrar Receita</a>
                     </li>
-                    <form class="form-inline my-2 my-lg-0" method="get" action="{{BASE}}?url=busca" id="frmBusca">
-                      <input class="form-control ml-5" type="text" placeholder="O que você procura?" id="txtTermoBusca">
-                      <button class="btn btn-secondary my-2 my-sm-0 bg-danger" type="submit">Buscar</button>
+
+                    <form class="form-inline justify-content-center" method="get" action="{{BASE}}?url=busca" id="frmBusca">
+                      <div class="input-group">
+                        <input class="form-control ml-5" type="text" placeholder="O que você procura?" id="txtTermoBusca">
+                        <div class="input-group-append">
+                          <button class="btn btn-secondary bg-danger" type="submit">Buscar</button>
+                        </div>
+                      </div>
                     </form>
                   </ul>
                 </div>
